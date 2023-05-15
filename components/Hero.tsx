bfdedf300a035bd74f4b6865c199487054f9eef0
Bridger Tower <bridgertower@gmail.com>
2023-05-15 14:08:37 -0600

    Updated

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+	return (
+		<section className="bg-slate-100 flex justify-center items-center p-8">
+			<div id="container" className=" max-w-sc">
+				<h1></h1>
+			</div>
+		</section>
+	);
+};
+
+export default Header;
