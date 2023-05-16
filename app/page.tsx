ab025a28099d700fe38c7b01fb3bd840edb3833b
Bridger Tower <bridgertower@gmail.com>
2023-05-16 11:02:49 -0600

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import Header from '@/components/Header';
-import Hero from '@/components/Hero';
-import Logos from '@/components/Logos';
-import Text from '@/components/Text';
+import Disclaimer from "@/components/Disclaimer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Logos from "@/components/Logos";
+import Text from "@/components/Text";
 
 export default function Home() {
-	return (
-		<main>
-			<Header />
-			<Hero />
-			<Logos />
-			<Text />
-		</main>
-	);
+  return (
+    <main>
+      <Header />
+      <Hero />
+      <Logos />
+      <Text />
+      <Disclaimer />
+    </main>
+  );
 }
---
+++ components/Disclaimer.tsx
@@ -0,0 +1,32 @@
+const Disclaimer = () => {
+  return (
+    <section
+      id="hero"
+      className="bg-slate-50 flex justify-center items-center p-8"
+    >
+      <div
+        id="container"
+        className="max-w-screen-lg flex flex-col items-center gap-6"
+      >
+        <p className="text-xs">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Disclaimer;
